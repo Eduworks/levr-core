dff3f434328823ca301f8526e0d2f865ea3c9fcc
Ronald Ray <fritz.ray@eduworks.com>
2015-05-14 16:55:49 -0700

    Resolved imports
    Distributed work

--- levr-core/src/java/com/eduworks/resolver/CommandInterface.java
+++ levr-core/src/java/com/eduworks/resolver/CommandInterface.java
@@ -1,12 +1,5 @@
 package com.eduworks.resolver;
 
-import java.io.InputStream;
-import java.util.Map;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.eduworks.resolver.Context;
 
 public interface CommandInterface extends Resolvable{
 
